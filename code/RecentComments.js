a87afc6243c90ca253b1c2ef924a741bb6bc3ed9
xysrgithub <52027367+xysrgithub@users.noreply.github.com>
2020-02-07 11:29:00 +0700

    Update RecentComments.js

--- code/RecentComments.js
+++ code/RecentComments.js
@@ -1,26 +1,4 @@
-function mementomag_recent_comment(a) {
-    var e, t = "a",
-        r = "b",
-        n = "c",
-        o = "l",
-        i = "m",
-        s = "o",
-        l = "p",
-        d = "s",
-        v = "t";
-    e = '<ul class="mementomag_recent_comment">';
-    for (var m = 0; numComments > m; m++) {
-        var c, f, g, A;
-        if (m == a.feed.entry.length) break;
-        e += "<li>";
-        for (var h = a.feed.entry[m], u = 0; u < h.link.length; u++) "alternate" == h.link[u].rel && (c = h.link[u].href);
-        for (var p = 0; p < h.author.length; p++) f = h.author[p].name.$t, g = h.author[p].gd$image.src;
-        g = -1 != g.indexOf("/s1600/") ? g.replace("/s1600/", "/s" + avatarSize + "-c/") : -1 != g.indexOf("/s220/") ? g.replace("/s220/", "/s" + avatarSize + "-c/") : -1 != g.indexOf("/s512-c/") && 0 != g.indexOf("") ? "" + g.replace("/s512-c/", "/s" + avatarSize + "-c/") : -1 != g.indexOf("blogblog.com/img/b16-rounded.gif") ? "https://cdn.statically.io/gh/xysrgithub/blogger/c54527df/img/user-s85.png" : -1 != g.indexOf("blogblog.com/img/openid16-rounded.gif") ? "https://cdn.statically.io/gh/xysrgithub/blogger/c54527df/img/user-s85.png" : -1 != g.indexOf("blogblog.com/img/blank.gif") ? -1 != defaultAvatar.indexOf("gravatar.com") ? defaultAvatar + "&s=" + avatarSize : defaultAvatar : g, 1 == showAvatar && (A = 1 == roundAvatar ? "avatarImage" : "", e += '<div class="avatarClass"><img class="' + A + '" src="' + g + '" alt="' + f + '" title="' + f + '" width="' + avatarSize + '" height="' + avatarSize + '"/></div>'), e += '<span>' + f + '</span> di <a class="author" target="_blank" title="' + f + '" href="' + c + '">' + posttext + "</a>";
-        var k = h.content.$t,
-            x = k.replace(/(<([^>]+)>)/gi, "");
-        "" != x && x.length > characters ? (x = x.substring(0, characters), x += "&hellip;", 1 == showMorelink && (x += '<a target="_blank" title="' + moreLinktext + '"href="' + c + '">' + moreLinktext + "</a>")) : x = x, e += "<span>" + x + "</span>", e += "</li>"
-    }
-    e += "</ul>", e += '<a style="font-size:0px" title="' + f + '"href="//m' + t + d + v + t + i + "van." + r + o + "og" + d + l + "ot." + n + s + i + '">' + i + t + d + " t" + t + i + "van.</a>";
-    var b = ".xxx";
-    1 == hideCredits && (b = "display:none;"), document.write(e)
-}
+function mementomag_recent_comment(h){var d='<ul class="mementomag_recent_comment">';for(var f=0;numComments>f;f++){var g,k;if(f==h.feed.entry.length)break;d+="<li>";for(var a=h.feed.entry[f],c=0;c<a.link.length;c++)"alternate"==a.link[c].rel&&(g=a.link[c].href);for(c=0;c<a.author.length;c++){var e=a.author[c].name.$t;var b=a.author[c].gd$image.src}b=-1!=b.indexOf("/s1600/")?b.replace("/s1600/","/s"+avatarSize+"-c/"):-1!=b.indexOf("/s220/")?b.replace("/s220/","/s"+avatarSize+"-c/"):-1!=b.indexOf("/s512-c/")&&
+0!=b.indexOf("")?""+b.replace("/s512-c/","/s"+avatarSize+"-c/"):-1!=b.indexOf("blogblog.com/img/b16-rounded.gif")?"https://cdn.statically.io/gh/xysrgithub/blogger/c54527df/img/user-s85.png":-1!=b.indexOf("blogblog.com/img/openid16-rounded.gif")?"https://cdn.statically.io/gh/xysrgithub/blogger/c54527df/img/user-s85.png":-1!=b.indexOf("blogblog.com/img/blank.gif")?-1!=defaultAvatar.indexOf("gravatar.com")?defaultAvatar+"&s="+avatarSize:defaultAvatar:b;1==showAvatar&&(k=1==roundAvatar?"avatarImage":
+"",d+='<div class="avatarClass"><img class="'+k+'" src="'+b+'" alt="'+e+'" title="'+e+'" width="'+avatarSize+'" height="'+avatarSize+'"/></div>');d+="<div>"+e+'</div> di <a class="author" target="_blank" title="'+e+'" href="'+g+'">'+posttext+"</a>";a=a.content.$t.replace(/(<([^>]+)>)/gi,"");""!=a&&a.length>characters&&(a=a.substring(0,characters),a+="&hellip;",1==showMorelink&&(a+='<a target="_blank" title="'+moreLinktext+'"href="'+g+'">'+moreLinktext+"</a>"));d+="<div>"+a+"</div>";d+="</li>"}document.write(d+
+'</ul><a style="font-size:0px" title="'+(e+'"href="//www.ekaawaludin.com">Eka Awaludin</a>'))};
